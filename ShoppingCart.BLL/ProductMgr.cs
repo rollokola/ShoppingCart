45f1803083b1c67ba34a33683544b180d2caa299
Kristofer Wiklund <kristofer.wiklund@prevas.se>
2018-02-14 09:42:40 +0100

    Add project files.

---
+++ ShoppingCart.BLL/ProductMgr.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShoppingCart.Contract.Generated;
+using ShoppingCart.DAL;
+
+namespace ShoppingCart.BLL
+{
+    public class ProductMgr
+    {
+        private ShoppingCartEntities _context;
+        public ProductMgr(ShoppingCart.DAL.ShoppingCartEntities context)
+        {
+            _context = context;
+
+        }
+        public List<Product> GetAll()
+        {
+            List<Product> result = null;
+         
+            result = _context.Product.ToList();
+
+            return result;
+        }
+
+
+        public Product Get(int id)
+        {
+            Product result = null;
+            result = _context.Product.FirstOrDefault();
+
+            return result;
+        }
+
+        public void Create(Product item)
+        {
+            _context.Product.Add(item);
+            _context.SaveChanges();
+        }
+
+        public void Update(Product item)
+        {
+            var dbItem = _context.Product.SingleOrDefault(x => x.Id == item.Id);
+            dbItem.Name = item.Name;
+            dbItem.Price = item.Price;
+            dbItem.Barcode = item.Barcode;
+            
+            _context.SaveChanges();
+        }
+
+        public void Remove(int id)
+        {
+            var post = _context.Product.SingleOrDefault(x => x.Id == id);
+            _context.Product.Remove(post);
+            _context.SaveChanges();
+        }
+
+    }
+}
---
+++ ShoppingCart.BLL/ShoppingListMgr.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShoppingCart.Contract.Generated;
+using ShoppingCart.DAL;
+
+namespace ShoppingCart.BLL
+{
+    public class ShoppingListMgr
+    {
+        private ShoppingCartEntities _context;
+        public ShoppingListMgr(ShoppingCart.DAL.ShoppingCartEntities context)
+        {
+            _context = context;
+
+        }
+        public void RemoveAll()
+        {
+            
+
+            var items =  _context.ShoppingList;
+            _context.ShoppingList.RemoveRange(items);
+            _context.SaveChanges();
+
+        }
+
+        public IEnumerable<ShoppingList> GetAll(int user)
+        {
+            List<ShoppingList> result = null;
+
+            result = (from x in  _context.ShoppingList
+                     where x.UserId == user
+                     select x).ToList();
+
+            return result;
+        }
+
+
+        public void Create(ShoppingList item)
+        {
+            _context.ShoppingList.Add(item);
+            _context.SaveChanges();
+        }
+
+        public void Remove(int id)
+        {
+            var post = _context.ShoppingList.SingleOrDefault(x => x.Id == id);
+            _context.ShoppingList.Remove(post);
+            _context.SaveChanges();
+        }
+
+
+        public void Update(ShoppingList item)
+        {
+            var dbItem = _context.ShoppingList.SingleOrDefault(x => x.Id == item.Id);
+            dbItem.Name = item.Name;
+            dbItem.UserId = item.UserId;
+           
+
+            _context.SaveChanges();
+        }
+
+    }
+}
